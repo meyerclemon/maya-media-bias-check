8539b693c1b1ae456b8aaef860c6676f8ea32c65
Student <example@example.com>
2019-07-19 17:48:04 -0700

    add jquery

--- background.js
+++ background.js
@@ -1,8 +1,14 @@
 
-var elementsInsideBody = [...document.body.getElementsByTagName('*')];
+
 
 let links = document.getElementsByTagName('a');
-for (elt of links) {
-elt.style['color'] = '#FF00FF';
-}
-console.log(links);
+$( "a:contains('nytimes.com')" ).css( "color", "green" );
+
+
+// var elementsInsideBody = [...document.body.getElementsByTagName('*')];
+
+// let links = document.getElementsByTagName('a');
+// for (elt of links) {
+// elt.style['color'] = '#FF00FF';
+// }
+// console.log(links);
