952a1071ab42316cda8c26d51313811dafae26a6
Student <example@example.com>
2019-07-20 18:58:10 -0700

    try to dry up code with an array in indexOf. It's not working

--- background.js
+++ background.js
@@ -1,7 +1,17 @@
 var links = [...document.body.getElementsByTagName("a")];
+var green = ["nytimes.com"];
+var red = ["foxnews.com"]
+
 for (var i = 0; i < links.length; i++) {
-  if (links[i]["href"].indexOf("www.nytimes.com") == -1) {
+  if (links[i]["href"].indexOf(green) == -1) {
     continue;
   }
   links[i].style["color"] = "green";
+}
+
+for (var i = 0; i < links.length; i++) {
+  if (links[i]["href"].indexOf(red) == -1) {
+    continue;
+  }
+  links[i].style["color"] = "red";
 }
