b24d91e4193120f33f066228f5bf9306fdf26ace
Student <example@example.com>
2019-07-26 16:52:39 -0700

    add more sources

--- infoBox.js
+++ infoBox.js
@@ -1,15 +1,21 @@
 $(document).ready(function(){
     $("#criteria").submit(function(event){
 
-if ((x > -18 && x < 18) && (y >= 45 && y <= 64)) {
+if ((x >= -18 && x < 18) 
+&& (y >= 45 && y <= 64)) {
     return green;
 }
 
-if((x > -24 && x < -24) && (y >= 21 && y <= 52)){
+if((x > -24 && x < 24) 
+&& (y >= 21 && y <= 53)){
     return yellow;
 }
 
-if((x >= -42 && x <= 24) && (y > 24 && y < 52) || (x <= 42 && x >= -24) && (y > 24 && y < 52) ||  (y > 16 && y < 24 )) {
+if((x >= -42 && x <= -24) 
+&& (y >= 24 && y < 52) 
+|| (x <= 42 && x >= 24) 
+&& (y >= 24 && y < 52) 
+||  (y >= 16 && y < 24 )) {
     return orange;
 }
 
