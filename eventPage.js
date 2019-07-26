47244950801bdc8514134c3298803eed158555bf
Student <example@example.com>
2019-07-26 12:36:59 -0700

    fix typo on eventPage so context menu popup now loads and directs to url

--- eventPage.js
+++ eventPage.js
@@ -1,25 +1,28 @@
 var contextMenuItem = {
-    "id": "checkMediaBias",
-    "title": "CheckMediaBias",
-    "contexts": ["selection"]
+  id: "checkMediaBias",
+  title: "CheckMediaBias",
+  contexts: ["selection"]
 };
 chrome.contextMenus.create(contextMenuItem);
 
 function fixedEncodeURI(str) {
-    return encodeURI(str).replace(/%5B/g, '[').replace(/%5D/g, ']')
+  return encodeURI(str)
+    .replace(/%5B/g, "[")
+    .replace(/%5D/g, "]");
 }
 
-chrome.contextMenus.onClicked.addListener(function (clickData){
-    if(clickData.menuItemId == "checkMediaBias" && clickeData.selectionText){
-        var moreInfo = "https://en.wikipedia.org/wiki"+ fixedEncodeURI(clickData.selectionText);
-        var createData = {
-            "url": moreInfo,
-            "type": "popup",
-            "top": 5,
-            "left": 5,
-            "width": parseInt(screen.availWidth / 2),
-            "height": parseInt(screen.availHeight / 2)
-        };
-        chrome.windows.create(createData, function(){});
-    }
+chrome.contextMenus.onClicked.addListener(function(clickData) {
+  if (clickData.menuItemId == "checkMediaBias" && clickData.selectionText) {
+    var moreInfo =
+      "https://en.wikipedia.org/wiki" + fixedEncodeURI(clickData.selectionText);
+    var createData = {
+      url: moreInfo,
+      type: "popup",
+      top: 5,
+      left: 5,
+      width: parseInt(screen.availWidth / 2),
+      height: parseInt(screen.availHeight / 2)
+    };
+    chrome.windows.create(createData, function() {});
+  }
 });
